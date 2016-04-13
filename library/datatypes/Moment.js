b031f98c8ac49be47c652042672a6c2b97ec920a
Bjørn Rosell <it@rosell.dk>
2016-04-13 11:12:01 +0200

    Moment.js is not a wrapper anymore

--- library/datatypes/Moment.js
+++ library/datatypes/Moment.js
@@ -5,7 +5,16 @@ Requires moment.js! (momentjs.com)
 momentjs is the actual data type. 
 This file is just adds functions which works on this datatype
 
+With moment, you can set the default format like this:
+moment.defaultFormat = 'MM-DD-YYYY';
+
 */
+
+//alert(moment.prototype.toString);
+
+//moment.defaultFormat = 'MM-DD-YYYY';
+
+//moment.defaultFormat 
 
 /**
  *  A whole bunch of signatures are supported by moment
@@ -14,6 +23,8 @@ This file is just adds functions which works on this datatype
  *  MOMENT() - Returns moment object representing now
  *  MOMENT([String]) - Not recommended
  *  MOMENT(date [String], format [String]) - Parse date using format. Ie: MOMENT("12-25-1995", "MM-DD-YYYY")
+ *  MOMENT(date [String], format [String], strictParsing [Boolean]) - optionally parse in strict mode
+ *  MOMENT(date [String], format [String], locale [String], strictParsing [Boolean]) - optionally format with a locale
  *  ...
  * 
  *  Check out the API here: http://momentjs.com/docs/#/parsing/
@@ -29,6 +40,10 @@ Formula.addFunction('MOMENT', function(a, b, c, d) {
 
 
 /**
+ * Format moment
+ * @param momentObj [Moment] - the Moment to format
+ * @param format [String] (optional) - The format. If no format is supplied, Moment.defaultFormat will be used
+ *
  * Examples: 
  *   'MMMM Do YYYY, h:mm:ss a' -> 'April 12th 2016, 12:59:32 pm'
  *   'dddd' -> 'Tuesday'
@@ -37,7 +52,7 @@ Formula.addFunction('MOMENT', function(a, b, c, d) {
  */
 Formula.addFunction('MOMENT_FORMAT', function(momentObj, format) {
   if (!(momentObj instanceof moment)) {
-    return typeof momentObj;
+    return 'Wrong type supplied to MOMENT_FORMAT:' + typeof momentObj;
   }
   return momentObj.format(format);
 });
@@ -50,16 +65,26 @@ Formula.addFunction('MOMENT_PARSE', function(text, format) {
 });
 
 /**
+ * Use moments strict parser
+ */
+Formula.addFunction('MOMENT_PARSE_STRICT', function(text, format) {
+  return new moment(text, format, true);
+});
+
+/**
  *  Strict parse, which only accepts strict compliance to the format
- *  Iee MOMENT_STRICT_PARSE('12-24-2014', 'MM-DD-YYYY') is accepted
+ *  Ie. MOMENT_STRICT_PARSE('12-24-2014', 'MM-DD-YYYY') is accepted
  *  but MOMENT_STRICT_PARSE('2-24-2014', 'MM-DD-YYYY') is not accepted
  *  and so is MOMENT_STRICT_PARSE('banana 2/24 2014', 'banana M/DD YYYY')
  *
  *  NOTE: ONLY the following tokens are supported: MM, DD, YYYY, D, M 
  */
-Formula.addFunction('MOMENT_TEST_DATE_FORMAT', function(text, format) {
+Formula.addFunction('MOMENT_CAN_PARSE_STRICT', function(text, format) {  
 //  return new moment(text, format);
   var f = format;
+  if (f === undefined) {
+    f = Moment.defaultFormat;
+  }
   f = f.replace('MM', '((0[1-9])|(1[012]))');         // accept 01 - 12
   f = f.replace('DD', '((0[1-9])|([12]\\d)|3[01])');  // accept 01 - 31
   f = f.replace('YYYY', '\\d\\d\\d\\d');  // accept 0000 - 9999
@@ -79,11 +104,19 @@ Formula.addFunction('MOMENT_TEST_DATE_FORMAT', function(text, format) {
 
 
 
-/** Return unit of date
+/** Return year, month, or whatever unit of a date
  *  Units available: 'year', 'month', 'date', 'hour', 'minute', 'second', 'millisecond'
+ *  http://momentjs.com/docs/#/get-set/set/
  */
 Formula.addFunction('MOMENT_GET', function(momentObj, unit) {
   return momentObj.get(unit);
+});
+
+/** Set year, month, or whatever unit of a date
+ *  Units available: 'year', 'month', 'date', 'hour', 'minute', 'second', 'millisecond'
+ */
+Formula.addFunction('MOMENT_SET', function(momentObj, unit, value) {
+  return momentObj.set(unit, value);
 });
 
 /** Return day of month (1-31) */

46eecdbdcf4710291656efed5578f4e358530c40
Bjørn Rosell <it@rosell.dk>
2016-04-14 11:36:52 +0200

    Added $$ function

--- library/datatypes/JQuery.js
+++ library/datatypes/JQuery.js
@@ -1,5 +1,26 @@
+
 Formula.addFunction('$', function(a, b) {
   return $(a,b);
+});
+
+
+Formula.addFunction('$$', function(a, b) {
+  
+  var $jq = $(a,b);
+  var formula = this.formula;
+
+  function changeHandler() {
+    formula.triggerChangeCallback();
+  }
+
+  $jq.each(function () {
+    if (!(jQuery.data($(this).get(0), 'added-handler'))) { 
+      $(this).on("change", changeHandler);
+      jQuery.data($(this).get(0), 'added-handler', true);
+    }
+  })
+
+  return $jq;
 });
 
 Formula.addFunction('$VAL', function($jq) {

063bd1f16b7455ea2db126560c62c304e6346fa8
Bjørn Rosell <it@rosell.dk>
2016-04-18 09:46:23 +0200

    rewrote parser completely. It now parses from left to right

--- formula.js
+++ formula.js
@@ -16,7 +16,9 @@ var Formula = function(formula, resultChangedCallBack) {
   }, 100);
 
   this.formulaFragment = Formula.parseFormula(formula, this);
-  this.formulaFragment.addChangeHandlers(this);
+  if (this.formulaFragment !== undefined) {
+    this.formulaFragment.addChangeHandlers(this);
+  }
 }
 
 Formula.prototype.triggerChangeCallback = function() {
@@ -26,6 +28,9 @@ Formula.prototype.triggerChangeCallback = function() {
 }
 
 Formula.prototype.calc = function() {
+  if (this.parseError()) {
+    return;
+  }
   return this.formulaFragment.calc();
 }
 
@@ -34,113 +39,214 @@ Formula.prototype.referenceChanged = function(reference) {
   this.resultChangedCallBack();
 }
 
-/**
- *  Return a FormulaFragment object with the parsed formula.
- *  This can later be executed with the .calc() method
- */
-Formula.parseFormula = function(formula, formulaObj) {
-  var s = 'PASSTHROUGH(' + formula + ')';
-  var formulas = []; 
-  /* 
-  Strategy is to parse last function found in string, until there are no more functions to be found.
-  Finding the last function is easier than the first, because it cannot possibly have child functions.
-  Finding the matching ")" for the function is as easy as finding the first ")" after the function name
-  On the other hand, if we were going for the outemost function, we would have trouble in strings like
-  these: "MULTIPLY(SUM(1,2),DIVIDE(2,3))". Finding the matching ")" of MULTIPLY amounts to finding the
-  last ")" in the string. But finding the matching ")" of SUM is the first. Thus, we would have had to 
-  count parentheses.
-
-  The last function will It will always be (one of) the innermost functions.
-  Each time we find a function, we:
-    1) create a Formula 
-    2) replace the function in the string with a placeholder: FORMULA-[N]
-    
-  In the end of this procedure, the resulting string will for example be:
-    Begin   "MULTIPLY(SUM(1,2),DIVIDE(2,3))"
-    Step 1  "MULTIPLY(SUM(1,2),FORMULA-1)"      formula1 = {fname:'DIVIDE', param: [2,3]}
-    Step 2  "MULTIPLY(FORMULA-2,FORMULA-1)"     formula2 = {fname:'SUM', param: [1,2]}
-    Step 3  "FORMULA-3"                         formula3 = {fname:'MULTIPLY', param: [formula1,formula2]}
-
-  But first, find mark all function calls, so we dont have to iterate
-  function names repeatedly
-  We here transform "MULTIPLY(SUM(1,2),2)" to "[FUNCTION:MULTIPLY](FUNCTION:SUM](1,2),2,)"
-  
- */
-  
-  for (var i=0; i<Formula.function_names.length; i++) {
-    var fname = Formula.function_names[i];
-    var re = new RegExp('(\\b|\\(|\\s)' + fname.replace(/\$/g, '\\$') + '\\(', 'g');
-    s = s.replace(re, '$1[FUNCTION:' + fname.replace(/\$/g, '$$$$') + '](');
-    if (fname == '$') {
-//      alert(s)
-    }
-  }
-  while (true) {
-    // Find last function
-    var fnPos = -1;
-    var fname = '';
-
-    var fnPos = s.lastIndexOf('[FUNCTION:');
-    if (fnPos == -1) {
-      return formulas[formulas.length-1];
-    }
-    var fnPos2 = s.indexOf(']', fnPos);
-    fname = s.substring(fnPos + 10, fnPos2);
-
-    var fnPos3 = s.indexOf(')', fnPos2);
-    var param = s.substring(fnPos2 + 2, fnPos3);
-
-    var parameters = param.split(',');
-
-    var resolvedParameters = [];
-
-    parameterloop:
-    for (var i=0; i<parameters.length; i++) {
-      var p = parameters[i].trim();
-
-      // Handle when the parameter is a function
-      if (p.indexOf('[FORMULA-') == 0) {
-        var formulaIndex = parseInt(p.substring(9, p.length-1), 0);
-        resolvedParameters.push(formulas[formulaIndex]);
-      }
-
-      else {
-
-        // Handle when the parameter is a reference
-        // - we loop through known prefixes to check if parameter starts with that prefix
-        for (var j=0; j<Formula.referencePrefixes.length; j++) {
-          var prefix = Formula.referencePrefixes[j];
-          if (p.indexOf(prefix) == 0) {
-            var references = Formula.referenceCreators[prefix](p.substr(prefix.length));
-            if (typeof references['getValue'] === 'function') {
-              resolvedParameters.push(references);
-            }
-            else {
-              for (var k=0; k<references.length; k++) {
-                resolvedParameters.push(references[k]);
-              };
-            }
-            continue parameterloop;
-          }
-        }
-
-        for (var j=0; j<Formula.parsers.length; j++) {
-          var parseResult = Formula.parsers[j](p);
-          if (parseResult !== undefined) {
-            resolvedParameters.push(parseResult);
-            continue;
-          }
-        }
-      }
-
-    }
-    
-    formulas.push(new Formula.Fragment(fname, resolvedParameters, parameters, formulaObj));
-
-    s = s.substr(0, fnPos) + '[FORMULA-' + (formulas.length-1) + ']' + s.substr(fnPos3+1);
-
-  }
+Formula.prototype.parseError = function() {
+  return (this.formulaFragment === undefined);
 }
+
+Formula.parseFormula = function(formula, formulaObj) {
+  console.log('parsing formula:' + formula)
+
+  var pos;
+  var functionLevel = 0;
+
+  // We eat up the formula from left to right
+  // meal contains what is not yet eaten
+  var meal = formula.trim();
+
+  // Output is an array of parsed tokens
+  var output = []
+
+  // RegEx's for sniffing
+  var reSniffFunction = /^([A-Z_\$]+[0-9A-Z_\$]*)\(/
+  var reSniffNumber = /^[-+]?[0-9]/
+  
+  // RegEx's for parsing
+  var reParseNumber = /^([-+]?(?:[0-9]+[.][0-9]+)|(?:[0-9]+))/
+  var reFindString = /^"((?:[^"]|"")*)"/
+  var reUndoublequoteString = /""/
+
+  var i=0;
+
+  mainloop:
+  while ((meal != '') && (i<99)) {
+    meal = meal.trimLeft();
+
+    i++
+    // parse function start
+    if (reSniffFunction.test(meal)) {
+      functionLevel++;
+
+      var fname = reSniffFunction.exec(meal)[1];
+      if (!(Formula.functions[fname])) {
+        console.log('Function: ' + fname + ' has not been defined. Parse error!');
+        return;
+      }
+      output.push(-1);   // -1: FUNCTION_START
+      output.push(fname)
+      meal = meal.substr(meal.indexOf('(') + 1);
+      continue;
+    }
+
+    // parse number
+    if (reSniffNumber.test(meal)) {
+      var result = reParseNumber.exec(meal);
+      console.log('parsing number' + meal);
+      console.log(result);
+      if (result == null) {
+        return;
+      }
+      var n = parseFloat(result[1]);
+      if (isNaN(n)) {
+        return;
+      }
+      output.push(-4);   // -4: Simple value
+      output.push(n);
+      meal = meal.substr(result[0].length);
+      continue;
+    }
+
+    // In Excell, only doublequotes can be used for strings. Same here
+    if (meal[0] == '"') {
+
+      // doublequotes are escaped with two doublequotes ie: "They call me ""the guy"""
+      // we must skip those
+      var result = reFindString.exec(meal);
+      if (result == null) {
+        console.log('Parser found something that looked like a string, but wasnt. Parse error!');
+        return;
+      }
+      var str = result[1].replace(/""/g, '"');
+      output.push(-4);   // -4: Simple value
+      output.push(str);
+      console.log('string value:' + str);
+
+      meal = meal.substr(result[0].length);
+      continue;
+    }
+
+    // Our build in parsers haven't found anything.
+    // Hand leftovers to the custom parsers
+    var isAtEndOfFunction = ((functionLevel > 0) && ((meal[0] == ',') || (meal[0] == ')')));
+
+    if (!isAtEndOfFunction) {
+
+      // Eat away, till next ',' or ')'
+  //    var re = /^([^,\)]+)[,\)]/
+      var re = /^([^,\)]+)(?:$|[,\)])/
+      var result = re.exec(meal);
+      if (result == null) {
+        return;
+      }
+      var text = result[1];
+console.log('leftovers:' + text);
+
+      meal = meal.substr(text.length);
+console.log('meal left:' + meal);
+
+      for (var j=0; j<Formula.parsers.length; j++) {
+        var parseResult = Formula.parsers[j](text);
+        if (parseResult !== undefined) {
+          // A match!
+          output.push(-4);   // -4: Simple value
+          output.push(parseResult);
+          continue mainloop;
+        }
+      }
+
+      // Alas. No match at all. This is a parse error
+      return
+    }
+
+
+    if (functionLevel > 0) {
+      // we should now be at a "," or a ")"
+      if (meal[0] == ',') {
+        meal = meal.substr(1);
+      }
+      else if (meal[0] == ')') {
+        functionLevel--;
+        output.push(-2);   // -2: FUNCTION_END;
+        meal = meal.substr(1);
+      }
+      else {
+        return null;
+      }
+    }
+  }
+
+  console.log('output:')
+  console.log(output);
+
+  // Next, process output to create Formula.Fragments
+  var i=0;
+
+  // If formula is just a simple value, wrap the whole formula it in a dummy function
+  // This is needed because Formula.Fragment expects a function
+  if (output[0] == -4) {
+    output.unshift(-1, 'PASSTHROUGH');
+    output.push(-2);
+  }
+
+  // When called i must point to Simple value, or FUNCTION_START
+  // When returning, i points to element after
+  function processExpression() {
+    if (output[i] == -1) {  // -1: FUNCTION_START
+      var fragment = processFunction();
+      if (fragment === undefined) {
+        return
+      }
+      return fragment;
+    }
+    else if (output[i] == -4) { // -4: Simple value
+      i=i+2;
+      return output[i-1];
+    }
+    else {
+      console.log('Unexpected token. Expected FUNCTION_START or SIMPLE_VALUE. Got: ' + output[i] + '. exiting');
+      return;
+    }
+  }
+
+  // When called, i must point to FUNCTION_START
+  // When returning, i points to element after
+  function processFunction() {
+    if (output[i] != -1) {
+      console.log('Unexpected token. Expected FUNCTION_START. exiting');
+      return;
+    }
+    i++;
+    var fname = output[i];
+    console.log('fname:' + fname);
+    i++;
+    var parameters = [];
+
+    // grap arguments
+    while (output[i] != -2) { // -2: FUNCTION_END
+
+      // grap argument
+      parameters.push(processExpression());
+      if (parameters[parameters.length - 1] === undefined) {
+        return;
+      }
+    }
+    i++;
+    console.log('Parsed function: ' + fname + '. Parameters:');
+    console.log(parameters);
+    return new Formula.Fragment(fname, parameters, formulaObj)
+  }
+
+  var frag = processFunction();
+  console.log('frag:');
+  console.log(frag);
+  return frag;
+
+// new Formula.Fragment(fname, parameters, parameters, formulaObj)
+
+//  console.log('meal:' + meal);
+
+//  alert(isFunction(0));
+}
+
 
 /* Functionality for adding functions */
 Formula.function_names = [];
@@ -178,25 +284,6 @@ Formula.addParser = function(parserFn, weight) {
 }
 
 
-// Add String parser
-Formula.addParser(function(text) {
-  // Test if starts with ' or " - for quick exit
-  if ((text[0] != "'") && (text[0] != '"')) {
-    return
-  }
-  // Test if end-quote matches start-quote
-  if (text[0] == text[text.length - 1]) {
-    return text.substring(1, text.length-1);
-  }
-});
-
-// Add Number parser
-Formula.addParser(function(text) {
-  if (!isNaN(parseFloat(text))) {
-    return parseFloat(text);
-  }
-});
-
 // Add Boolean parser
 Formula.addParser(function(text) {
   if (text.toLowerCase() == 'false') {
@@ -214,9 +301,8 @@ Formula.addParser(function(text) {
 /* Class Formula.Frament */
 /* --------------------- */
 
-Formula.Fragment = function(fname, resolvedParameters, parameters, formulaObj) {
+Formula.Fragment = function(fname, parameters, formulaObj) {
   this.fname = fname;
-  this.resolvedParameters = resolvedParameters;
   this.parameters = parameters;
   this.formulaObj = formulaObj;
 }
@@ -228,8 +314,8 @@ Formula.Fragment.prototype.calc = function() {
   // - formula => result of formula
 
   var values = [];
-  for (var i=0; i<this.resolvedParameters.length; i++) {
-    var p = this.resolvedParameters[i];
+  for (var i=0; i<this.parameters.length; i++) {
+    var p = this.parameters[i];
     if (p instanceof Formula.Fragment) {
       p = p.calc();
     }
@@ -249,8 +335,8 @@ Formula.Fragment.prototype.calc = function() {
 
 /* Add change handlers to all references in this formula */
 Formula.Fragment.prototype.addChangeHandlers = function(formula) {
-  for (var i=0; i<this.resolvedParameters.length; i++) {
-    var p = this.resolvedParameters[i];
+  for (var i=0; i<this.parameters.length; i++) {
+    var p = this.parameters[i];
     if (p instanceof Formula.Fragment) {
       p.addChangeHandlers(formula);
     }

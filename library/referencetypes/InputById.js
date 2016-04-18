8329478dc86a6d04fa14cbdb12a55e0324158300
Bjørn Rosell <it@rosell.dk>
2016-04-18 09:49:41 +0200

    rewrote to use new prefered way of adding reference types

--- library/referencetypes/InputById.js
+++ library/referencetypes/InputById.js
@@ -1,18 +1,26 @@
 // Note: jQuery required!
 
 /**
- *  Reference INPUT fields by ID with "#"
+ *  Get the value of an INPUT field, referenced by element ID.
+ *  Ie if you have <input id="bananas" value="3">, you can reference the value like this: #banana, Ie CONCAT("You have eaten ", #bananas, " bananas")
  */
-
-function InputById(ref) {
-  this.elm = document.getElementById(ref);
-}
-InputById.prototype.getValue = function() {
-  return $(this.elm).val();
-}
-InputById.prototype.setChangeCallback = function(changeCallback) {
-  $(this.elm).bind("change", changeCallback);
-}
-Formula.addReferenceType('#', function (ref){
-  return new InputById(ref)
+Formula.addParser(function(text) {
+  if (text[0] != '#') {
+    return
+  }
+  var elm = document.getElementById(text.substr(1));
+  if (elm) {
+    // Return a "Bound variable". (an object with a getValue() function, and a setChangeCallback() function
+    return {
+      getValue: function () {
+        return elm.value;
+      },
+      setChangeCallback: function(changeCallback) {
+        // This is where jQuery is needed.
+        // TODO: do not depend on jQuery
+        $(elm).bind("change", changeCallback);
+      }
+    }
+  }
 });
+
